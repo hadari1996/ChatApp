5ba4fc6ee319f5b28ec652dd69ec4c6d93a3feb3
hadari1996 <hadarzocaim@gmail.com>
2023-08-02 18:45:53 +0300

    allowedOrigins fix

--- server/config/allowedOrigins.ts
+++ server/config/allowedOrigins.ts
@@ -1,6 +1,4 @@
-export let allowedOrigins: string | any[];
-
-allowedOrigins = [
+export const allowedOrigins = [
   "https://chatapp-v3zs.onrender.com",
   "https://chatapp-server-iq5u.onrender.com",
   "http://localhost:4000",
