0e846c27e461052062696fd25872b71f0b755448
hadari1996 <hadarzocaim@gmail.com>
2023-09-19 23:20:39 +0300

    cors fix2

--- server/config/allowedOrigins.ts
+++ server/config/allowedOrigins.ts
@@ -3,4 +3,5 @@ export const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "https://chatapp-v3zs.onrender.com/MyAvatar",
+  "https://chatapp-server-iq5u.onrender.com"
 ];
