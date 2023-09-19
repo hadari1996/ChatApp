aba27b1894c569f24cf2025ccf3cd454d48755d1
hadari1996 <hadarzocaim@gmail.com>
2023-09-19 23:18:06 +0300

    cors fix

--- server/server.ts
+++ server/server.ts
@@ -16,7 +16,7 @@ const httpServer = createServer(app);
 const MONGO_URI = process.env.MONGO_URI;
 mongoose.set("strictQuery", true);
 app.use(express.json());
-app.use(cors(corsOptions));
+
  app.use(cors());
 
 
