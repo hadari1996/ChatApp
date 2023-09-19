ce92785e918cd00ddf0c976a99929c076ff91ed9
hadari1996 <hadarzocaim@gmail.com>
2023-09-19 23:25:33 +0300

    cors open

--- server/config/allowedOrigins.ts
+++ server/config/allowedOrigins.ts
@@ -1,7 +1,7 @@
-export const allowedOrigins = [
-  "https://chatapp-v3zs.onrender.com",
-  "http://localhost:3000",
-  "http://localhost:3001",
-  "https://chatapp-v3zs.onrender.com/MyAvatar",
-  "https://chatapp-server-iq5u.onrender.com"
+export const allowedOrigins = ["*"
+  // "https://chatapp-v3zs.onrender.com",
+  // "http://localhost:3000",
+  // "http://localhost:3001",
+  // "https://chatapp-v3zs.onrender.com/MyAvatar",
+  // "https://chatapp-server-iq5u.onrender.com"
 ];
