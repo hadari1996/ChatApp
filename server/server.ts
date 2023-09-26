7d390f28d1706f9c23e8ed7893557abe2bd5edf9
hadari1996 <hadarzocaim@gmail.com>
2023-09-26 20:40:16 +0300

    express json

--- server/server.ts
+++ server/server.ts
@@ -16,7 +16,7 @@ const PORT = process.env.PORT;
 const httpServer = createServer(app);
 const MONGO_URI = process.env.MONGO_URI;
 mongoose.set("strictQuery", true);
-app.use(express.json());
+// app.use(express.json());
 
 
 
